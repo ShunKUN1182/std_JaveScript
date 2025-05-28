0f3bf83f34758fd634f5902162812225b4c29d97
ShunKUN1182 <2250378@ecc.ac.jp>
2025-05-28 22:04:26 +0900

    お試し：色々と触ってみたが電卓へのみちは遠そう、、少し眠いので今日は大人しく寝て明日頑張ってみる。

--- js/js/hw_01.js
+++ js/js/hw_01.js
@@ -1,1 +1,18 @@
 console.log("san");
+
+const keisan=document.querySelector("#keisan");
+
+const keisan2=document.querySelector("#keisan2");
+
+const kigou=document.querySelector("#kigou");
+
+console.log(keisan);
+
+const namA=keisan;
+const namB=keisan2;
+let mof;
+mof=keisan+keisan2;
+
+
+
+console.log(mof);
