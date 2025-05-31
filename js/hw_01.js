642b8d25f7b46ab51623f5b83b376c9843340623
ShunKUN1182 <2250378@ecc.ac.jp>
2025-05-31 22:01:23 +0900

    末尾にeventで持ってきた数値がくるように設定

--- js/hw_01.js
+++ js/hw_01.js
@@ -1,8 +1,10 @@
 const buttons = document.querySelectorAll('button');
 const keisan = document.querySelector('keisan')
 
+let gangan="";
 function buttonPresseed(event) {
     const text = event.target.textContent;
-    alert(text);
+    gangan = gangan+text;
+    console.log(gangan);
 }
 buttons.forEach(button => button.addEventListener('click',buttonPresseed))
