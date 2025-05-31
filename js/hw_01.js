6edccd6b2c2a853fc5549dc08df90cfe4d467b5d
ShunKUN1182 <2250378@ecc.ac.jp>
2025-05-31 17:53:48 +0900

    検索：ネットで電卓の作り方を調べつつ、わからない属性や文字列を調べながらわかるところまで書いてみた。 eval属性を設定してボタンが押されたとき、中のテキストをとってくるというイベントを設定。 動作するかアラートでチェック済み。 if文を使えば分岐でうまいことなんかできないかな、、？

--- js/hw_01.js
+++ js/hw_01.js
@@ -1,1 +1,8 @@
-console.log("san");
+const buttons = document.querySelectorAll('button');
+const keisan = document.querySelector('keisan')
+
+function buttonPresseed(event) {
+    const text = event.target.textContent;
+    alert(text);
+}
+buttons.forEach(button => button.addEventListener('click',buttonPresseed))
