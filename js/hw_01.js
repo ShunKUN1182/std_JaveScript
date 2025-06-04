3911c832f5f8118866b6822606019297fa41f6f8
ShunKUN1182 <2250378@ecc.ac.jp>
2025-06-04 14:40:20 +0900

    リセット：一旦スクリプトリセット！！ ちゃんと理解して書く

--- js/hw_01.js
+++ js/hw_01.js
@@ -1,23 +1,4 @@
-const buttons = document.querySelectorAll('button');
-const keisan = document.querySelector("#keisan");
+const numA=document.querySelectorAll(".num")
+const result=document.querySelector("#keisan")
 
-let concatText="";
-function buttonPresseed(event) {
-    const text = event.target.textContent;
-
-    if(text === "="){
-        concatText = eval(concatText);
-    }else if(text === "C"){
-        concatText= "";
-    } else {   
-        concatText = concatText+text;
-    }
-    
-    keisan.textContent = concatText;
-
-    }
-
-
-
-buttons.forEach(button => button.addEventListener('click',buttonPresseed))
 
