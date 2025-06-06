28222bc54e7e8bce9f4bf9a0b7a2fa21cb1e08ce
ShunKUN1182 <2250378@ecc.ac.jp>
2025-06-06 18:29:17 +0900

    文字を打つときは””必須、 かくにんはlogで、 なるべくidなどを決めるときは英語でしっかり。

--- html/js/hw01_02.js
+++ html/js/hw01_02.js
@@ -1,11 +1,17 @@
-console.log("hosokawa");
+const No1=document.querySelector("#inputNo1")
+const No2=document.querySelector("#inputNo2")
+const btn=document.querySelector("#calculation")
+const rlt=document.querySelector("#result")
 
+console.log("rlt");
 
 
 // const text1=document.querySelector("#iti")
         // const text2=document.querySelector("#ni")
         // const button=document.querySelector("button")
         // const result=document.querySelector("#result")
+// こんなまちがいをしないようにしよう
+
 
 
         // function buttonpresseed(event){
