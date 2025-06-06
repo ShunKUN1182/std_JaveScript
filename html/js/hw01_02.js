9e52f384cefd228f83a5ba1c6d48dc0e8b5a3796
ShunKUN1182 <2250378@ecc.ac.jp>
2025-06-06 18:12:17 +0900

    ファイルまとめ

---
+++ html/js/hw01_02.js
@@ -0,0 +1,17 @@
+console.log("hosokawa");
+
+
+
+// const text1=document.querySelector("#iti")
+        // const text2=document.querySelector("#ni")
+        // const button=document.querySelector("button")
+        // const result=document.querySelector("#result")
+
+
+        // function buttonpresseed(event){
+            
+        //     let keisan;
+        //     keisan=text1+text2;
+        //     console.log(keisan);
+            
+        // }
