914f13f53795c02a0b47880fb1100645ac914a4e
ShunKUN1182 <2250378@ecc.ac.jp>
2025-06-11 18:38:23 +0900

    サンプル終了

---
+++ js/sample06.js
@@ -0,0 +1,31 @@
+const text = document.querySelector("#text");
+
+
+text.innerHTML = "あいう<br>えお";
+
+const userName = document.querySelector("#name")
+const addBtn = document.querySelector("#addBtn")
+const memberLists = document.querySelector(".memberList>tbody")
+
+// console.log(userName,addBtn,memberLists);
+
+addBtn.addEventListener("click",() =>{
+
+    let memberList = "<tr>";
+    memberList += `<td>1</td>`;
+    memberList += `<td></td>`;
+    memberList +="</tr>";
+
+})
+
+
+
+
+// memberLists.innerHTML = "<tr><td>1</td><td>坂口晃平</td></tr>"
+
+// 人を追加できるように
+// Noが毎回変わるようにする
+// 全消しボタンの追加→tbodyは残す
+// 個別削除の追加
+// 空の場合はエラー
+// 性別モードの実装
