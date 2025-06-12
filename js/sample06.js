354c4fcc403569905a712f028e88fdc822da6a9b
ShunKUN1182 <2250378@ecc.ac.jp>
2025-06-12 11:10:13 +0900

    進捗：ボタンを押すたびにNoが増えるようになった

--- js/sample06.js
+++ js/sample06.js
@@ -6,11 +6,17 @@ const memberLists = document.querySelector(".memberList>tbody")
 
 // console.log(userName,addBtn,memberLists);
 
+let num = 0;
+
+function clk() {
+    num++;
+
+};
 
 addBtn.addEventListener("click",() =>{
       
     let memberList = `<tr>`;
-    memberList += `<td>1</td>`;
+    memberList += `<td>${num}</td>`;
     memberList += `<td>${userName.value}</td>`;
     memberList +="</tr>";
     memberLists.innerHTML+=memberList
@@ -22,8 +28,8 @@ addBtn.addEventListener("click",() =>{
 
 // memberLists.innerHTML = "<tr><td>1</td><td>坂口晃平</td></tr>"
 
-// 人を追加できるように
-// Noが毎回変わるようにする
+// 人を追加できるように（クリア！）
+// Noが毎回変わるようにする（くりあ！）
 // 全消しボタンの追加→tbodyは残す
 // 個別削除の追加
 // 空の場合はエラー
