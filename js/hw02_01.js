5803284ad3ccf37bfe4e9a6296cf2dc46f1d5649
ShunKUN1182 <2250378@ecc.ac.jp>
2025-06-16 23:21:58 +0900

    乱数ができた！！！

--- js/hw02_01.js
+++ js/hw02_01.js
@@ -3,4 +3,16 @@ const opponentTeam = document.querySelector("#opponentTeam")
 const playBtn = document.querySelector("#playBtn")
 const matchList = document.querySelector("#matchList>tbody")
 
-console.log(homeTeam,opponentTeam,playBtn,matchList);
+// console.log(homeTeam,opponentTeam,playBtn,matchList);
+
+// let random = Math.random();
+
+// console.log(random);
+// ↑これで０〜１の乱数ができる、これに最大値をかけて使う
+
+
+
+let random = Math.floor(Math.random()*11);
+
+
+
