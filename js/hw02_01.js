1f6990b9183752cfd174ace94dbbb5fdbee88b96
ShunKUN1182 <2250378@ecc.ac.jp>
2025-06-16 23:47:50 +0900

    課題はまだあるが完成

--- js/hw02_01.js
+++ js/hw02_01.js
@@ -12,20 +12,29 @@ const matchLists = document.querySelector("#matchList>tbody")
 
 
 
-let random = Math.floor(Math.random()*11);
+let randomA = Math.floor(Math.random()*11);
+let randomB = Math.floor(Math.random()*11);
 
 
 playBtn.addEventListener("click",()=>{
     let matchList = `<tr>`
     matchList += `<td>`
-    matchList += `${homeTeam.value}`
-    matchList += `</td>`
-    matchList += `<td>`
+    matchList += `${homeTeam.value} vs `
     matchList += `${opponentTeam.value}`
     matchList += `</td>`
     matchList += `<td>`
-    matchList += `勝利`
+    matchList += `${randomA}-${randomB}`
+    matchList += `</td>`
+    matchList += `<td>`
+    if (randomA>randomB) {
+        matchList +=`勝利`
+    }else if (randomA<randomB) {
+        matchList +=`敗北`
+    }else{
+        matchList +=`引き分け`
+    }
     matchList += `</td>`
     matchList += `</tr>`
-    matchLists.innerHTML = matchList
+    matchLists.innerHTML += matchList
+
 })
