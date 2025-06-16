996a7bcf93bf11d6bc43f3ec0db4c7803f1cd459
ShunKUN1182 <2250378@ecc.ac.jp>
2025-06-16 23:07:41 +0900

    コンストで持ってきた、ログで確認

---
+++ js/hw02_01.js
@@ -0,0 +1,6 @@
+const homeTeam = document.querySelector("#homeTeam")
+const opponentTeam = document.querySelector("#opponentTeam")
+const playBtn = document.querySelector("#playBtn")
+const matchList = document.querySelector("#matchList>tbody")
+
+console.log(homeTeam,opponentTeam,playBtn,matchList);
