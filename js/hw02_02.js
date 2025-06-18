172c0bea69c1d6c4f5e1f60b5cf719eb4e884dad
ShunKUN1182 <2250378@ecc.ac.jp>
2025-06-18 17:23:40 +0900

    完成

--- js/hw02_02.js
+++ js/hw02_02.js
@@ -1,8 +1,9 @@
-const homeTeam = document.querySelector("#homeTeam")
-const opponentTeam = document.querySelector("#opponentTeam")
-const playBtn = document.querySelector("#playBtn")
-const matchLists = document.querySelector("#matchList>tbody")
-const winPct = document.querySelector("#winPct")
+const homeTeam = document.querySelector("#homeTeam");
+const opponentTeam = document.querySelector("#opponentTeam");
+const playBtn = document.querySelector("#playBtn");
+const matchLists = document.querySelector("#matchList>tbody");
+const winPct = document.querySelector("#winPct");
+const matchWinPct = document.querySelector("#matchWinPct");
 
 // console.log(homeTeam,opponentTeam,playBtn,matchList);
 
@@ -10,10 +11,19 @@ const winPct = document.querySelector("#winPct")
 
 // console.log(random);
 // ↑これで０〜１の乱数ができる、これに最大値をかけて使う
-let totalMatch = 143;
+const totalMatchNum = 143;
+let totalMatch =totalMatchNum ;
 let matchNum = 1;
 let result = "引き分け";
-let totolResult = [0,0,0];
+// let totalResults = [0,0,0];
+let totalResults = [];
+totalResults["win"]=0;
+totalResults["lose"]=0;
+totalResults["draw"]=0;
+
+
+
+
 
 playBtn.addEventListener("click",()=>{
     while(totalMatch > 0){
@@ -42,7 +52,7 @@ playBtn.addEventListener("click",()=>{
             result = `敗北`;
             matchList += result;
         }else{
-            matchList += `<td class="drowP">`;
+            matchList += `<td class="drawP">`;
             result = `引き分け`;
             matchList += result;
         }
@@ -59,23 +69,39 @@ playBtn.addEventListener("click",()=>{
 
         if (result === "勝利") {
             matchLists.querySelector("tr:last-child").classList.add("winP");
+            totalResults["win"]+= 1;
+
         }else if (result === "敗北") {
             matchLists.querySelector("tr:last-child").classList.add("loseP");
+            totalResults["lose"]+= 1;
+
         }else if (result === "引き分け") {
-            matchLists.querySelector("tr:last-child").classList.add("drowP");
+            matchLists.querySelector("tr:last-child").classList.add("drawP");
+            totalResults["draw"]+= 1;
+
         }else{
             matchLists.querySelector("tr:last-child").classList.add("err");
         }
         
         
         const loseP = document.querySelectorAll(".loseP");
-        const drowP = document.querySelectorAll(".drowP");
+        const drawP = document.querySelectorAll(".drawP");
         
-        // console.log(winP,loseP,drowP);
+        // console.log(winP,loseP,drawP);
         
-        let winPt = `100%`;
-        winPct.innerHTML = winPt;
         totalMatch--;
         matchNum++;
+
     }
+    // let totalResult = totalResults["win"];
+    // totalResult += "勝";
+    // totalResult += totalResults["lose"];
+    // totalResult += "敗";
+    // totalResult += totalResult["draw"];
+    // totalResult += "分"
+    matchWinPct.innerHTML = `${totalResults["win"]}勝${totalResults["lose"]}敗${totalResults["draw"]}分`;
+    let winPt = Math.trunc((totalResults["win"]/(totalMatchNum-totalResults["draw"]))*100);
+    console.log(winPt);
+    
+    winPct.innerHTML = `${winPt}%`
     })
