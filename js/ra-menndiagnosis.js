7656c53e0c99fae2d5d48b307eeed6d25c321974
ShunKUN1182 <2250378@ecc.ac.jp>
2025-06-21 22:29:28 +0900

    いったんわからない

--- js/ra-menndiagnosis.js
+++ js/ra-menndiagnosis.js
@@ -1,7 +1,20 @@
 const btn = document.querySelector("#btn");
-const q1 = document.getElementsByName("q1");
+let q1 = document.getElementsByName("q1").checked;
 const q2 = document.getElementsByName("q2");
 const q3 = document.getElementsByName("q3");
 
 // console.log(q1,q2,q3,btn);
 
+console.log(q1);
+
+
+btn.addEventListener("click",()=>{
+    
+    if (q1[1] === "true") {
+        console.log("こってり");  
+    }else{
+        console.log("あっさり");
+        
+    }
+
+})
