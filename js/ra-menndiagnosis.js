92d876df74255230600f9f71c1b50ff9c79a5c08
ShunKUN1182 <2250378@ecc.ac.jp>
2025-06-21 22:20:04 +0900

    muzukasii

--- js/ra-menndiagnosis.js
+++ js/ra-menndiagnosis.js
@@ -1,2 +1,7 @@
-const rich = 
-const btn = document.querySelector("#btn")
+const btn = document.querySelector("#btn");
+const q1 = document.getElementsByName("q1");
+const q2 = document.getElementsByName("q2");
+const q3 = document.getElementsByName("q3");
+
+// console.log(q1,q2,q3,btn);
+
