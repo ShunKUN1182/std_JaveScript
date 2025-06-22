7e7ac617ff6c53a7b287408f972710def47e06e0
ShunKUN1182 <2250378@ecc.ac.jp>
2025-06-22 10:15:55 +0900

    分岐完成

--- js/ra-menndiagnosis.js
+++ js/ra-menndiagnosis.js
@@ -1,20 +1,47 @@
 const btn = document.querySelector("#btn");
-let q1 = document.getElementsByName("q1").checked;
+let q1 = document.getElementsByName("q1");
 const q2 = document.getElementsByName("q2");
 const q3 = document.getElementsByName("q3");
+const result = document.querySelector("h3");
 
 // console.log(q1,q2,q3,btn);
 
-console.log(q1);
-
-
 btn.addEventListener("click",()=>{
-    
-    if (q1[1] === "true") {
-        console.log("こってり");  
+
+    // console.log(q1[0].checked);
+
+    if (q1[0].checked === true) {
+        if (q2[0].checked === true) {
+            if (q3[0].checked === true) {
+                console.log("天下一品");
+            }else{
+                console.log("燃えよメンスケ");
+            }
+        }else{
+            if (q3[0].checked === true) {
+                console.log("人類みな麺類");
+            }else{
+                console.log("小麦の女神");
+            }
+        }
     }else{
-        console.log("あっさり");
-        
+        if (q2[0].checked === true) {
+            if (q3[0].checked === true) {
+                console.log("怪力屋");
+            }else{
+                console.log("来来亭");
+            }
+        }else{
+            if (q3[0].checked === true) {
+                console.log("ラーメン人生");
+            }else{
+                console.log("太鼓");
+                
+            }
+        }
     }
 
 })
+
+
+
