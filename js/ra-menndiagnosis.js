5135202d536664ba2629d80b7f5bc0bb60a62106
ShunKUN1182 <2250378@ecc.ac.jp>
2025-06-22 10:33:18 +0900

    途中

--- js/ra-menndiagnosis.js
+++ js/ra-menndiagnosis.js
@@ -13,29 +13,29 @@ btn.addEventListener("click",()=>{
     if (q1[0].checked === true) {
         if (q2[0].checked === true) {
             if (q3[0].checked === true) {
-                result.innerHTML = "鳥の鶏次"
+                result.innerHTML = "鳥の鶏次";
             }else{
-                result.innerHTML = "無鉄砲"
+                result.innerHTML = "無鉄砲";
             }
         }else{
             if (q3[0].checked === true) {
-                result.innerHTML = "ラーメン人生jet"
+                result.innerHTML = "ラーメン人生jet";
             }else{
-                result.innerHTML = "小麦の麺神"
+                result.innerHTML = "たけ井";
             }
         }
     }else{
         if (q2[0].checked === true) {
             if (q3[0].checked === true) {
-                console.log("怪力屋");
+                result.innerHTML = "ニューヨーク×ニューヨーク";
             }else{
-                console.log("来来亭");
+                result.innerHTML = "カドヤ食堂";
             }
         }else{
             if (q3[0].checked === true) {
-                console.log("ラーメン人生");
+                result.innerHTML = "坂本龍馬が愛したラーメン食堂";
             }else{
-                console.log("太鼓");
+                result.innerHTML = "燃えよ麺助"
                 
             }
         }
