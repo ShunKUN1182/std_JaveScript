1690f006ba575fbf61c850956ef385f3649ef2aa
ShunKUN1182 <2250378@ecc.ac.jp>
2025-06-25 10:52:38 +0900

    とちゅ

---
+++ js/sample07.js
@@ -0,0 +1,10 @@
+const btn = document.querySelector("#btn");
+const tableA = document.querySelector("#timesTable>tbody")
+
+btn.addEventListener("click",()=>{
+    
+    for (let i = 0; i < 8; i++) {
+        
+        
+    }
+})
