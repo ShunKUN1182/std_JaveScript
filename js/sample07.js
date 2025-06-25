315c04611223c85f4c27c22ab90f1977cfbec833
ShunKUN1182 <2250378@ecc.ac.jp>
2025-06-25 11:01:07 +0900

    むずい

--- js/sample07.js
+++ js/sample07.js
@@ -1,10 +1,39 @@
 const btn = document.querySelector("#btn");
-const tableA = document.querySelector("#timesTable>tbody")
+const tableA = document.querySelector("#timesTable>tbody");
 
 btn.addEventListener("click",()=>{
     
-    for (let i = 0; i < 8; i++) {
-        
-        
+    for (let i = 0; i < 9; i++) {
+        let tableB = "<tr>"
+        tableB += "<td>"
+        tableB += "1"
+        tableB += "</td>"
+        tableB += "<td>"
+        tableB += "1"
+        tableB += "</td>"
+        tableB += "<td>"
+        tableB += "1"
+        tableB += "</td>"
+        tableB += "<td>"
+        tableB += "1"
+        tableB += "</td>"
+        tableB += "<td>"
+        tableB += "1"
+        tableB += "</td>"
+        tableB += "<td>"
+        tableB += "1"
+        tableB += "</td>"
+        tableB += "<td>"
+        tableB += "1"
+        tableB += "</td>"
+        tableB += "<td>"
+        tableB += "1"
+        tableB += "</td>"
+        tableB += "<td>"
+        tableB += "1"
+        tableB += "</td>"
+        tableB += "</tr>"
+        tableA.innerHTML = tableB;
     }
+
 })
