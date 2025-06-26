f08d352ccaf61775cd70b39106880975708707f7
ShunKUN1182 <2250378@ecc.ac.jp>
2025-06-26 16:37:30 +0900

    要素をとれた

--- js/kuku.js
+++ js/kuku.js
@@ -1,6 +1,8 @@
 const btn = document.querySelector("#btn");
 const tableA = document.querySelector("#timesTable>tbody");
 let tableB = "";
+let tableC = "";
+const style = document.querySelector("style")
 
 
 
@@ -16,15 +18,23 @@ btn.addEventListener("click",()=>{
 
         for(let j = 1; j <= 9; j++){
             
-            tableB += `<td>`;
+            tableB += `<td class="a${j} b${i}">`;
             tableB += `${i*j}`;
             tableB += `</td>`;
 
         }
         
         tableB += `</tr>`;
+        // tableC = `.a:hover{`
+        // tableC += `background-color: aqua;`
+        // tableC += `}`
     }
     
     tableA.innerHTML += tableB;
+    // style.innerHTML += tableC
+
+    for (let k = 1; k <= 9; k++) {
+        const a = document.querySelectorAll(`.a${k}`) 
+    }
 
 })
