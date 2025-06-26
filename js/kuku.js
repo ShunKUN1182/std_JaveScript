7c9970b1c7b17a98a94fcf3313a8f86e84e23111
ShunKUN1182 <2250378@ecc.ac.jp>
2025-06-26 14:54:32 +0900

    なぜかできない

--- js/kuku.js
+++ js/kuku.js
@@ -2,3 +2,26 @@ const btn = document.querySelector("#btn");
 const tableA = document.querySelector("#timesTable>tbody");
 
 
+
+btn.addEventListener("click",()=>{
+
+    let tableB = "";
+
+    for (let i = 1; i => 9; i++){
+
+        tableB += `<tr>`;
+
+        for(let j = 1; j => 9; j++){
+            
+            tableB += `<td>`;
+            tableB += `${i*j}`;
+            tableB += `</td>`;
+            
+        }
+        
+        tableB += `</tr>`;
+    }
+
+tableA.innerHTML += tableB;
+
+})
