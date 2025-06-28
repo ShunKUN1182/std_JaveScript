a4893b4163f965a51591199ba7ff3ccae68b9143
ShunKUN1182 <2250378@ecc.ac.jp>
2025-06-28 14:05:58 +0900

    ファイル整理

---
+++ js/sample08.js
@@ -0,0 +1,29 @@
+const randomNums = [];
+let setNums = new Set();
+const maxNum = 5;
+const limit = 10;
+
+while(setNums.size<maxNum){
+    randomNums.push(Math.floor(Math.random() * limit)+1);
+    setNums = new Set(randomNums);
+}
+
+// randomNumber配列をsetNumsからコピー
+const randomNumber = [...setNums];
+
+const sortNumber = [];
+
+console.log("ランダムな数字",randomNumber);
+/*
+step1
+js の sort を使って実現
+*/
+
+
+/*
+step2
+配列操作で自作する
+*/
+
+console.log("並び替えたあとの数字",sortNumber);
+
