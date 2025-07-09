d2803276e7fc7a7672cc0e8b23cd4d0fd1903390
ShunKUN1182 <2250378@ecc.ac.jp>
2025-07-09 19:37:49 +0900

    一旦終了

--- js/sample10.js
+++ js/sample10.js
@@ -1,1 +1,19 @@
 console.log("foo");
+const btns = document.querySelectorAll(".btn");
+
+
+
+// btns.addEventListener("click",()=>{
+//     console.log("saka");
+// })
+
+function changeBackground(color="#FFF") {
+    document.body.style.backgroundColor = color;
+}
+
+btns.forEach((e) => {
+    e.addEventListener("click",(event)=>{
+        console.dir(event.target.dataset.color);
+        changeBackground(event.target.dataset.color);
+    })
+});
