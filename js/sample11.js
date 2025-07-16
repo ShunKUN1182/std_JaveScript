49fca3beceec66bdab05271654752ff0079862ea
ShunKUN1182 <2250378@ecc.ac.jp>
2025-07-16 18:55:19 +0900

    サンプル終了

--- js/sample11.js
+++ js/sample11.js
@@ -1,1 +1,29 @@
-console.log("foo");
+const thumbnailImages = document.querySelectorAll(".thumbnailWrap > p > img");
+const modal = document.querySelector(".modalWrap");
+
+console.log(modal);
+
+thumbnailImages.forEach(e => {
+    e.addEventListener("click" , (event)=>{
+        // console.log("foo");
+        // console.log(event.target);
+        console.log(modal.classList.contains("modalClose"));
+        
+        if (modal.classList.contains("modalClose")) {
+            modal.classList.add("modalOpan");
+            modal.classList.remove("modalClose");
+        }
+    })
+});
+
+modal.addEventListener("click" , ()=>{
+    // console.log("foo");
+    
+    if (modal.classList.contains("modalOpan")) {
+        // console.log("foo");
+        
+        modal.classList.add("modalClose");
+        modal.classList.remove("modalOpan");
+    }
+
+})
