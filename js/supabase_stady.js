dd752aeddcba6da51029ab845e1c4b618fefc165
ShunKUN1182 <2250378@ecc.ac.jp>
2025-11-26 19:35:07 +0900

    すごいですこれ

--- js/supabase_stady.js
+++ js/supabase_stady.js
@@ -1,18 +1,24 @@
 const supabaseUrl = "https://jmbbgtqiiwwcnfyokiam.supabase.co";
 const supabaseKey = "sb_publishable_0oQmgJP5Q9S3QnAqfxgZLA_QsuFsR1i";
-const supabaseClient = window.supabase.createClient(supabaseUrl, supabaseKey);
+const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
 const inputText = document.querySelector("input");
 const btn = document.querySelector("button");
+const list = document.querySelector("ul");
 
 async function loadData() {
-    const { data, error } = await supabaseClient.from("ramen").select("*");
+    const { data, error } = await supabase.from("ramen").select("*");
 
     console.log("data:", data);
     console.log("error:", error);
+    for (let i = 0; i < data.length; i++) {
+        list.insertAdjacentHTML("beforeend", `<li> ${i} : ${data[i].title}</li>`);
+    }
 }
 
 async function insertTest() {
     const { error } = await supabase.from("ramen").insert({ title: inputText.value });
+    console.log("無事成功しました");
+    console.log(error);
 }
 
 btn.addEventListener("click", () => {
