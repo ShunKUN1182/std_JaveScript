5fa04709fe2936eb91a8f7bcf50a4ea520c8a3d4
ShunKUN1182 <2250378@ecc.ac.jp>
2026-01-16 19:31:37 +0900

    apiの勉強

--- .history/js/supabase_stady_20251207224752.js
+++ .history/js/supabase_stady_20251207224752.js
@@ -5,6 +5,16 @@ const inputText = document.querySelector("input");
 const btn = document.querySelector("button");
 const list = document.querySelector("ul");
 
+async function loadData() {
+    const { data, error } = await supabase.from("ramen").select("*");
+    console.log("data:", data);
+    console.log("error:", error);
+    for (let i = 0; i < data.length; i++) {
+        list.insertAdjacentHTML("beforeend", `<li> ${i + 1} : ${data[i].title}</li>`);
+        list.insertAdjacentHTML("beforeend", `<img src="${data[i].image_url}">`);
+    }
+}
+
 async function insertTest() {
     const { error } = await supabase.from("ramen").insert({ title: inputText.value });
     console.log("無事成功しました");
