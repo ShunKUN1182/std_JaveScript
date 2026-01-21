f72e5358f39bece29b97e9cbc17fe7111c6cf244
ShunKUN1182 <2250378@ecc.ac.jp>
2026-01-21 09:34:44 +0900

    apiをjsonファイル形式で取得

--- frontend_test/js/index.js
+++ frontend_test/js/index.js
@@ -1,1 +1,9 @@
 const url = "https://click.ecc.ac.jp/ecc/webdesign/wdct/api/v1/schewd/";
+
+async function callApi() {
+    const apiDatas = await fetch(url);
+    const users = await apiDatas.json();
+    console.log(users);
+}
+
+callApi();
