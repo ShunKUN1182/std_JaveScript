e3f487e8b799ad8ab9d4584cfe1570990573886d
ShunKUN1182 <2250378@ecc.ac.jp>
2026-01-22 14:38:08 +0900

    javascriptを使って出力

--- js/pokemon.js
+++ js/pokemon.js
@@ -1,4 +1,4 @@
-const pokemonInput = document.querySelector("p");
+const pokemonInput = document.querySelector(".pokemons");
 const url = "https://pokeapi.co/api/v2/pokemon-species/pikachu";
 
 async function pokemonApi() {
@@ -17,7 +17,38 @@ const no1Data = {
     type: ["grass", "poison"],
     height: 0.7,
     weight: 6.9,
-    img: "pokemonImg / pokemon01.png",
+    img: "pokemonImg/pokemon01.png",
 };
 
+function pokemonOutput() {
+    let pokemonOutput = "";
+    pokemonOutput += `<div class="pokemon">`;
+    pokemonOutput += `<div class="pokemonBackground">`;
+    pokemonOutput += `<img src="${no1Data.img}" alt="${no1Data.name}" />`;
+    pokemonOutput += `</div>`;
+    pokemonOutput += `<div class="pokemonText">`;
+    pokemonOutput += `<div class="noPokemon">`;
+    pokemonOutput += `<p>No.${no1Data.num}</p>`;
+    pokemonOutput += `<div>`;
+    no1Data.type.forEach((e) => {
+        pokemonOutput += `<span class="${e}">${e}</span>`;
+    });
+    pokemonOutput += `</div>`;
+    pokemonOutput += `</div>`;
+    pokemonOutput += `<h2>フシギダネ</h2>`;
+    pokemonOutput += `<div>`;
+    pokemonOutput += `<p>高さ:</p>`;
+    pokemonOutput += `<p>${no1Data.height}m</p>`;
+    pokemonOutput += `</div>`;
+    pokemonOutput += `<div>`;
+    pokemonOutput += `<p>重さ:</p>`;
+    pokemonOutput += `<p>${no1Data.weight}kg</p>`;
+    pokemonOutput += `</div>`;
+    pokemonOutput += `</div>`;
+    pokemonOutput += `</div>`;
+
+    pokemonInput.innerHTML += pokemonOutput;
+}
+
+pokemonOutput();
 // pokemonApi();
