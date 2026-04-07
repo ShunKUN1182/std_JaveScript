2dc308332ac72c1b43da1e14e7193c1e37194216
ShunKUN1182 <2250378@ecc.ac.jp>
2026-04-07 14:32:11 +0900

    style: hintを作成

--- oseroGame/js/index.js
+++ oseroGame/js/index.js
@@ -4,277 +4,341 @@ let setCell = [
     {
         bgc: "light",
         setStone: 0,
+        hint: "",
     },
     {
         bgc: "dark",
         setStone: 0,
+        hint: "",
     },
     {
         bgc: "light",
         setStone: 0,
+        hint: "",
     },
     {
         bgc: "dark",
         setStone: 0,
+        hint: "",
     },
     {
         bgc: "light",
         setStone: 0,
+        hint: "",
     },
     {
         bgc: "dark",
         setStone: 0,
+        hint: "",
     },
     {
         bgc: "light",
         setStone: 0,
+        hint: "",
     },
     {
         bgc: "dark",
         setStone: 0,
+        hint: "",
     },
     {
         bgc: "dark",
         setStone: 0,
+        hint: "",
     },
     {
         bgc: "light",
         setStone: 0,
+        hint: "",
     },
     {
         bgc: "dark",
         setStone: 0,
+        hint: "",
     },
     {
         bgc: "light",
         setStone: 0,
+        hint: "",
     },
     {
         bgc: "dark",
         setStone: 0,
+        hint: "",
     },
     {
         bgc: "light",
         setStone: 0,
+        hint: "",
     },
     {
         bgc: "dark",
         setStone: 0,
+        hint: "",
     },
     {
         bgc: "light",
         setStone: 0,
+        hint: "",
     },
     {
         bgc: "light",
         setStone: 0,
+        hint: "",
     },
     {
         bgc: "dark",
         setStone: 0,
+        hint: "",
     },
     {
         bgc: "light",
         setStone: 0,
+        hint: "",
     },
     {
         bgc: "dark",
         setStone: 0,
+        hint: "hint",
     },
     {
         bgc: "light",
         setStone: 0,
+        hint: "",
     },
     {
         bgc: "dark",
         setStone: 0,
+        hint: "",
     },
     {
         bgc: "light",
         setStone: 0,
+        hint: "",
     },
     {
         bgc: "dark",
         setStone: 0,
+        hint: "",
     },
     {
         bgc: "dark",
         setStone: 0,
+        hint: "",
     },
     {
         bgc: "light",
         setStone: 0,
+        hint: "",
     },
     {
         bgc: "dark",
         setStone: 0,
-    },
-    {
-        bgc: "light",
-        setStone: 2,
-    },
-    {
-        bgc: "dark",
-        setStone: 1,
-    },
-    {
-        bgc: "light",
-        setStone: 0,
-    },
-    {
-        bgc: "dark",
-        setStone: 0,
-    },
-    {
-        bgc: "light",
-        setStone: 0,
-    },
-    {
-        bgc: "light",
-        setStone: 0,
-    },
-    {
-        bgc: "dark",
-        setStone: 0,
-    },
-    {
-        bgc: "light",
-        setStone: 0,
-    },
-    {
-        bgc: "dark",
-        setStone: 1,
+        hint: "hint",
     },
     {
         bgc: "light",
         setStone: 2,
+        hint: "",
     },
     {
         bgc: "dark",
-        setStone: 0,
+        setStone: 1,
+        hint: "",
     },
     {
         bgc: "light",
         setStone: 0,
+        hint: "",
     },
     {
         bgc: "dark",
         setStone: 0,
-    },
-    {
-        bgc: "dark",
-        setStone: 0,
+        hint: "",
     },
     {
         bgc: "light",
         setStone: 0,
-    },
-    {
-        bgc: "dark",
-        setStone: 0,
+        hint: "",
     },
     {
         bgc: "light",
         setStone: 0,
+        hint: "",
     },
     {
         bgc: "dark",
         setStone: 0,
+        hint: "",
     },
     {
         bgc: "light",
         setStone: 0,
+        hint: "",
+    },
+    {
+        bgc: "dark",
+        setStone: 1,
+        hint: "",
+    },
+    {
+        bgc: "light",
+        setStone: 2,
+        hint: "",
     },
     {
         bgc: "dark",
         setStone: 0,
+        hint: "hint",
     },
     {
         bgc: "light",
         setStone: 0,
-    },
-    {
-        bgc: "light",
-        setStone: 0,
+        hint: "",
     },
     {
         bgc: "dark",
         setStone: 0,
-    },
-    {
-        bgc: "light",
-        setStone: 0,
+        hint: "",
     },
     {
         bgc: "dark",
         setStone: 0,
+        hint: "",
     },
     {
         bgc: "light",
         setStone: 0,
+        hint: "",
     },
     {
         bgc: "dark",
         setStone: 0,
+        hint: "",
     },
     {
         bgc: "light",
         setStone: 0,
+        hint: "",
     },
     {
         bgc: "dark",
         setStone: 0,
-    },
-    {
-        bgc: "dark",
-        setStone: 0,
+        hint: "hint",
     },
     {
         bgc: "light",
         setStone: 0,
+        hint: "",
     },
     {
         bgc: "dark",
         setStone: 0,
+        hint: "",
     },
     {
         bgc: "light",
         setStone: 0,
+        hint: "",
+    },
+    {
+        bgc: "light",
+        setStone: 0,
+        hint: "",
     },
     {
         bgc: "dark",
         setStone: 0,
+        hint: "",
     },
     {
         bgc: "light",
         setStone: 0,
+        hint: "",
     },
     {
         bgc: "dark",
         setStone: 0,
+        hint: "",
     },
     {
         bgc: "light",
         setStone: 0,
+        hint: "",
+    },
+    {
+        bgc: "dark",
+        setStone: 0,
+        hint: "",
+    },
+    {
+        bgc: "light",
+        setStone: 0,
+        hint: "",
+    },
+    {
+        bgc: "dark",
+        setStone: 0,
+        hint: "",
+    },
+    {
+        bgc: "dark",
+        setStone: 0,
+        hint: "",
+    },
+    {
+        bgc: "light",
+        setStone: 0,
+        hint: "",
+    },
+    {
+        bgc: "dark",
+        setStone: 0,
+        hint: "",
+    },
+    {
+        bgc: "light",
+        setStone: 0,
+        hint: "",
+    },
+    {
+        bgc: "dark",
+        setStone: 0,
+        hint: "",
+    },
+    {
+        bgc: "light",
+        setStone: 0,
+        hint: "",
+    },
+    {
+        bgc: "dark",
+        setStone: 0,
+        hint: "",
+    },
+    {
+        bgc: "light",
+        setStone: 0,
+        hint: "",
     },
 ];
 
 for (let i = 0; i < setCell.length; i++) {
     if (setCell[i].setStone == 0) {
         board.innerHTML += `
-          <div class="cell ${setCell[i].bgc}">
+          <div class="cell ${setCell[i].bgc} ${setCell[i].hint}">
               <div class="stone"></div>
           </div>
         `;
     } else if (setCell[i].setStone == 1) {
         board.innerHTML += `
-          <div class="cell ${setCell[i].bgc}">
+          <div class="cell ${setCell[i].bgc} ${setCell[i].hint}">
               <div class="stone black"></div>
           </div>
         `;
     } else {
         board.innerHTML += `
-          <div class="cell ${setCell[i].bgc}">
+          <div class="cell ${setCell[i].bgc} ${setCell[i].hint}">
               <div class="stone white"></div>
           </div>
         `;
