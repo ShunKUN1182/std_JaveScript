1df7273e9888532d3f5466fcc0625d253676b9c6
ShunKUN1182 <2250378@ecc.ac.jp>
2026-05-22 18:41:37 +0900

    途中

--- react_std/src/App.jsx
+++ react_std/src/App.jsx
@@ -1,58 +1,37 @@
-import "./App.css";
 import { useState } from "react";
 
-const user = {
-    name: "fukusima",
-    age: 19,
-    course: "Web",
-};
-
-const wd2a = [
-    {
-        name: "さかぐち",
-        age: 19,
-        hobby: "ボートレース",
-    },
-    {
-        name: "ほそかわ",
-        age: 19,
-        hobby: "デザイン",
-    },
-    {
-        name: "ふくしま",
-        age: 19,
-        hobby: "ブロスタ",
-    },
-];
-
-function MyButton({ count, onClick }) {
-    return (
-        <>
-            <button onClick={onClick}>今 {count} 回やで</button>
-        </>
-    );
-}
-
-const listItems = wd2a.map((member) => (
-    <li key={member.name}>
-        私は{member.name}です。{member.age}歳です。{member.hobby}が好きです。
-    </li>
-));
-
-export default function App() {
-    const [count, setCount] = useState(0);
+function Square() {
+    const [value, setValue] = useState(null);
     function handleClick() {
-        setCount(count + 1);
+        setValue("X");
     }
 
     return (
+        <button className="square" onClick={handleClick}>
+            {value}
+        </button>
+    );
+}
+
+export default function Board() {
+    const [squares, setSquares] = useState(Array(9).fill(null));
+    return (
         <>
-            <div>
-                <h1>これは{user.name}のアプリです</h1>
-                <MyButton count={count} onClick={handleClick} />
-                <MyButton count={count} onClick={handleClick} />
+            <div className="board-row">
+                <Square value={squares[0]} />
+                <Square value={squares[1]} />
+                <Square value={squares[2]} />
             </div>
-            <ul>{listItems}</ul>
+            <div className="board-row">
+                <Square value={squares[3]} />
+                <Square value={squares[4]} />
+                <Square value={squares[5]} />
+            </div>
+            <div className="board-row">
+                <Square value={squares[6]} />
+                <Square value={squares[7]} />
+                <Square value={squares[8]} />
+            </div>
         </>
     );
 }
