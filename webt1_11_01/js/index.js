58195cc1997a6cb9b6fc3bcc9defaa151ccfc88d
ShunKUN1182 <2250378@ecc.ac.jp>
2026-06-29 16:57:10 +0900

    完成

--- webt1_11_01/js/index.js
+++ webt1_11_01/js/index.js
@@ -1,7 +1,7 @@
 const body = document.querySelector("body");
-const audio01 = new Audio("../audio/type01.mp3");
-const audio02 = new Audio("../audio/type02.mp3");
-const audio03 = new Audio("../audio/type03.mp3");
+const audio01 = new Audio("./audio/type01.mp3");
+const audio02 = new Audio("./audio/type02.mp3");
+const audio03 = new Audio("./audio/type03.mp3");
 
 body.addEventListener("keydown", (e) => {
     const keys = document.querySelectorAll(".key_wrap div");
@@ -9,8 +9,18 @@ body.addEventListener("keydown", (e) => {
         console.log(ele.textContent, e.code);
         const inputKey = e.code.split("y")[1];
         if (inputKey === ele.textContent) {
+            const randomNum = Math.floor(Math.random() * 3 + 1);
+            if (randomNum == 1) {
+                audio01.currentTime = 0;
+                audio01.play();
+            } else if (randomNum == 2) {
+                audio02.currentTime = 0;
+                audio02.play();
+            } else {
+                audio03.currentTime = 0;
+                audio03.play();
+            }
             ele.classList.add("active");
-            audio01.played();
             setTimeout(() => {
                 ele.classList.remove("active");
             }, 100);
