90b4a3e9683c2c9bd610b9336b139c91f9945340
ShunKUN1182 <2250378@ecc.ac.jp>
2026-07-30 18:17:00 +0900

    認証apiで情報ゲット

--- std_api/src/App.jsx
+++ std_api/src/App.jsx
@@ -1,13 +1,48 @@
 import "./App.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
+    const [user, setUser] = useState(null);
+    const URL = "https://dummyjson.com/auth/login";
 
-    function handleLogin(e) {
+    async function handleLogin(e) {
         e.preventDefault();
+        const response = await fetch(URL, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                username: username,
+                password: password,
+            }),
+        });
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data);
+            localStorage.setItem("accessToken", data.accessToken);
+            getUserData();
+        } else {
+            console.log("error");
+        }
     }
+
+    async function getUserData() {
+        const token = localStorage.getItem("accessToken");
+        const url = "https://dummyjson.com/auth/me";
+        const response = await fetch(url, {
+            method: "GET",
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+        });
+        const data = await response.json();
+        setUser(data);
+        console.log(data);
+    }
+
     return (
         <>
             <form onSubmit={handleLogin}>
@@ -27,6 +62,7 @@ function App() {
                 />
                 <button type="submit">ログイン</button>
             </form>
+            <h1>{user ? user.firstName + "さんようこそ！！" : "ログインしてください"}</h1>
         </>
     );
 }
